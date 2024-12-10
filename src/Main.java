83853b7ed519c14e7bd5f52a9e46924dc5942bd4
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-10 15:31:45 +0530

    UC4

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,36 @@ import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
         Scanner scanner = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-        System.out.println("Enter contact details:");
-        System.out.print("First Name: ");
-        String firstName = scanner.nextLine();
-        System.out.print("Last Name: ");
-        String lastName = scanner.nextLine();
-        System.out.print("Address: ");
-        String address = scanner.nextLine();
-        System.out.print("City: ");
-        String city = scanner.nextLine();
-        System.out.print("State: ");
-        String state = scanner.nextLine();
-        System.out.print("Zip: ");
-        int zip = scanner.nextInt();
-        System.out.print("Phone Number: ");
-        long phoneNumber = scanner.nextLong();
-        scanner.nextLine(); // Consume newline
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
-        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        addressBook.addContact(contact);
+        System.out.println("Welcome to Address Book Program");
+        boolean continueAdding = true;
+        while (continueAdding) {
+            System.out.println("\nEnter contact details:");
+            System.out.print("First Name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("Last Name: ");
+            String lastName = scanner.nextLine();
+            System.out.print("Address: ");
+            String address = scanner.nextLine();
+            System.out.print("City: ");
+            String city = scanner.nextLine();
+            System.out.print("State: ");
+            String state = scanner.nextLine();
+            System.out.print("Zip: ");
+            int zip = scanner.nextInt();
+            System.out.print("Phone Number: ");
+            long phoneNumber = scanner.nextLong();
+            scanner.nextLine(); // Consume newline
+            System.out.print("Email: ");
+            String email = scanner.nextLine();
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.addContact(contact);
+            System.out.print("\nDo you want to add another contact? (yes/no): ");
+            String response = scanner.nextLine();
+            continueAdding = response.equalsIgnoreCase("yes");
+        }
         System.out.println("\nAll Contacts:");
         addressBook.displayContacts();
-        System.out.print("\nEnter the first name of the contact to edit: ");
-        String editName = scanner.nextLine();
-        addressBook.editContact(editName);
-        System.out.print("\nEnter the first name of the contact to delete: ");
-        String deleteName = scanner.nextLine();
-        addressBook.deleteContact(deleteName);
     }
 }
